c7775c8c053b89921f2d20f36c99b10396f81e7a
atlxs <a.dmitrijoseph@gmail.com>
2017-08-04 13:30:53 -0400

    going to dc

--- draw_example.py
+++ draw_example.py
@@ -1,32 +1,44 @@
 import matplotlib.pyplot as plt
+import moviepy.editor as mpy
+from moviepy.video.io.bindings import mplfig_to_npimage
 from random import randint
+
+plt.ion()
+scatter_plots = []
 
 
 def rand_scatter(xval, yval, init_fig):
     if init_fig:
-        plt.ion()
-        f, ax = plt.subplots(1, figsize=(6 , 6))
+        f, ax = plt.subplots(1, figsize=(6, 6))
         im = ax.scatter(xval, yval)
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_xlim([0, 10])
         ax.set_ylim([0, 10])
     else:
-        plt.figure(1)
         ax = plt.gca()
         for coll in plt.gca().collections:
             coll.remove()
         im = ax.scatter(xval, yval)
+    plt.draw()
+    plt.pause(0.000001)
+    scatter_plots.append(mplfig_to_npimage(plt.gcf()))
 
-        plt.draw()
-        plt.pause(0.000001)
 
 if __name__ == "__main__":
     xval = []
     yval = []
     init = True
-    while 0 == 0:
+    i = 0
+    movie = []
+    while i != 10:
         xval.append(randint(1,9))
         yval.append(randint(1, 9))
+        #rand_boxplot(xval, yval, init)
         rand_scatter(xval, yval, init)
         init = False
+        i += 1
+
+    clip = mpy.ImageSequenceClip(scatter_plots, fps=1, load_images=True)  # Accepts folders
+    clip.write_videofile('draw_example.mp4', fps=1, codec='mpeg4', audio=False)
+
